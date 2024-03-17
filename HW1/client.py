5137a493e32a5bdc7a490010e644967813c1695c
alpdk <alexander@podkopaev.net>
2024-03-17 11:53:45 +0200

    Now clients receive average offset, and added some comments

--- HW1/client.py
+++ HW1/client.py
@@ -1,36 +1,46 @@
 import socket
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
+# Transform string to timedelta object
+def get_offset(offset: str):
+    datetime_obj = datetime.strptime(offset, "%H:%M:%S.%f")
+    total_microsec = datetime_obj.hour + datetime_obj.minute + datetime_obj.second + datetime_obj.microsecond
+    return timedelta(microseconds=total_microsec)
+
+
+# Run client node
 def run_client(server_ip, server_port):
-    # create a socket object
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # establish connection with server
     client.connect((server_ip, server_port))
+
+    offset = timedelta(seconds=0)
 
     try:
         while True:
-            msg = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            msg = (datetime.now() + offset).strftime("%d/%m/%Y %H:%M:%S")
 
             client.send(msg.encode("utf-8")[:1024])
 
             clock_time_string = client.recv(1024).decode("utf-8")
+            offset = get_offset(clock_time_string)
 
             if clock_time_string == "":
                 print(f"Server from {server_port[0]}:{server_port[1]} is closed", end="\n\n")
                 return
 
-            print("Received new time: " + clock_time_string + "\nCurrent time:     ", datetime.now(), end="\n\n")
+            print("Received offset time:", str(offset),
+                  "\nCurrent time:        ", datetime.now() + offset, end="\n\n")
 
             time.sleep(5)
     except Exception as e:
         print(f"Error: {e}", end="\n\n")
     finally:
-        # close client socket (connection to the server)
         client.close()
         print("Connection to server closed", end="\n\n")
+
 
 if __name__ == '__main__':
     run_client('127.0.0.1', 8080)
--- HW1/server.py
+++ HW1/server.py
@@ -3,47 +3,11 @@ import threading
 import time
 from datetime import datetime, timedelta
 
-# def handle_client(client_socket, addr):
-#     try:
-#         while True:
-#             # receive and print client messages
-#             request = client_socket.recv(1024).decode("utf-8")
-#             if request.lower() == "close":
-#                 client_socket.send("closed".encode("utf-8"))
-#                 break
-#             print(f"Received: {request}")
-#             # convert and send accept response to the client
-#             response = "accepted"
-#             client_socket.send(response.encode("utf-8"))
-#     except Exception as e:
-#         print(f"Error when handling client: {e}")
-#     finally:
-#         client_socket.close()
-#         print(f"Connection to client ({addr[0]}:{addr[1]}) closed")
-#
-# def run_server(server_ip, port):
-#     try:
-#         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         # bind the socket to the host and port
-#         server.bind((server_ip, port))
-#         # listen for incoming connections
-#         server.listen()
-#         print(f"Listening on {server_ip}:{port}")
-#
-#         while True:
-#             # accept a client connection
-#             client_socket, addr = server.accept()
-#             print(f"Accepted connection from {addr[0]}:{addr[1]}")
-#             # start a new thread to handle the client
-#             thread = threading.Thread(target=handle_client, args=(client_socket, addr,))
-#             thread.start()
-#     except Exception as e:
-#         print(f"Error: {e}")
-#     finally:
-#         server.close()
-
+# Clients data for sending average offset time
 client_data = {}
 
+
+# Collecting information from client nodes
 def client_time(client_socket, addr):
     try:
         while True:
@@ -69,8 +33,9 @@ def client_time(client_socket, addr):
     finally:
         client_socket.close()
 
-def accept_client(server):
 
+# Connect client node to server
+def accept_client(server):
     try:
         while True:
             client_socket, addr = server.accept()
@@ -84,6 +49,8 @@ def accept_client(server):
     finally:
         server.close()
 
+
+# Calculate averager offset time for client nodes
 def get_average_offset():
     time_difference_list = list(client['time_difference']
                                 for client_addr, client
@@ -98,17 +65,18 @@ def get_average_offset():
 
     return average_clock_difference
 
+
+# Send average offset to client nodes
 def start_change_time():
     try:
         while True:
             average_offset = get_average_offset()
 
             print("Average offset: ", average_offset, end="\n\n")
+            print(datetime.now())
 
             for client in client_data:
-                synchronized_time = datetime.now() + average_offset
-
-                client_data[client]['connector'].send(str(synchronized_time).encode("utf-8")[:1024])
+                client_data[client]['connector'].send(str(average_offset).encode("utf-8")[:1024])
 
                 print(f"Send update time for {client[0]}:{client[1]}", end="\n\n")
 
@@ -117,6 +85,7 @@ def start_change_time():
         print(f"Error: {e}", end="\n\n")
 
 
+# Run server node
 def run_server(server_ip, port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # bind the socket to the host and port
@@ -125,7 +94,7 @@ def run_server(server_ip, port):
     server.listen()
     print(f"Listening on {server_ip}:{port}", end="\n\n")
 
-    accept_threads = threading.Thread(target=accept_client, args=(server, ))
+    accept_threads = threading.Thread(target=accept_client, args=(server,))
     accept_threads.start()
 
     throw_threads = threading.Thread(target=start_change_time, args=())
