a58bf8a6353f6a88693b0c517ce8181a5e57796a
alpdk <alexander@podkopaev.net>
2024-05-09 11:35:02 +0300

    Added method for collecting user info from existing user

---
+++ HW2/file_management/getting_specific_user_info.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import json
+import pathlib
+
+def get_user_info(user_name):
+    path_to_dir = pathlib.Path().resolve().parent
+    path_to_user_folder = os.path.join(path_to_dir, "users_data", user_name)
+
+    if os.path.exists(path_to_user_folder):
+        path_to_user_info = os.path.join(path_to_user_folder, "user_info.json")
+
+        f = open(str(path_to_user_info), "r")
+        user_data = json.load(f)
+        f.close()
+
+        return user_data
