3a6808031c473ae1e1d47c537fb696c944d70260
alpdk <alexander@podkopaev.net>
2024-05-09 10:59:35 +0300

    Changed create user script

--- HW2/file_management/create_user.py
+++ HW2/file_management/create_user.py
@@ -1,6 +1,37 @@
 import os
 import sys
+import json
 import pathlib
+
+def write_in_port_to_hosts(path_to_dir, host, port):
+    port_to_sock_path = os.path.join(path_to_dir, "users_data", "port_to_hosts.json")
+
+    if not os.path.exists(port_to_sock_path):
+        with open(str(port_to_sock_path), "w") as file:
+            pass
+
+    f = open(port_to_sock_path, "r")
+    data_port_to_sock = json.load(f)
+    f.close()
+
+    if port in data_port_to_sock and not host in data_port_to_sock[port]:
+        data_port_to_sock[port].append(host)
+    else:
+        data_port_to_sock[port] = [host]
+
+    with open(str(port_to_sock_path), "w") as json_file:
+        json.dump(data_port_to_sock, json_file, indent=4)
+
+def write_user_info(path_to_dir, user_name, user_password, host, port):
+    user_info_path = os.path.join(path_to_dir, "users_data", user_name, "user_info.json")
+
+    user_info = {"name": user_name,
+                 "password": user_password,
+                 "host": host,
+                 "port": port}
+
+    with open(str(user_info_path), "w") as json_file:
+        json.dump(user_info, json_file, indent=4)
 
 if __name__ == '__main__':
     data = sys.argv[1:]
@@ -12,14 +43,17 @@ if __name__ == '__main__':
         print('Error: too much arguments')
         sys.exit(1)
 
-    path = pathlib.Path().resolve().parent
-    path = os.path.join(path, "users_data", data[0])
+    user_name = data[0]
+    user_password = data[1]
+    host = data[2]
+    port = data[3]
 
-    if not os.path.exists(path):
-        os.makedirs(path)
+    path_to_dir = pathlib.Path().resolve()
+    path_to_user_folder = os.path.join(path_to_dir, "users_data", user_name)
 
-        os.makedirs(os.path.join(path, 'data'))
+    if not os.path.exists(path_to_user_folder):
+        os.makedirs(path_to_user_folder)
 
-        f = open(pathlib.Path(str(path) + '/user_info'), "w")
-        f.write(f"{data[2]} {data[3]}")
-        f.close()
+        write_in_port_to_hosts(path_to_dir, host, port)
+
+        write_user_info(path_to_dir, user_name, user_password, host, port)
