f695d9410faf7f5cfc67b01791e6f2d086362866
slava233 <harik10346@gmail.com>
2024-05-10 09:14:56 +0300

    Container implementation for interaction between several pages

--- HW2/gui/main.py
+++ HW2/gui/main.py
@@ -1,86 +1,116 @@
+import os
 import pathlib
 import subprocess
-from tkinter import *
-import os
+import tkinter as tk
+from tkinter import ttk
 
 from HW2.file_management.getting_specific_user_info import get_user_info
 
 
-class UserAuthentication:
-    def __init__(self):
-        self.main_screen = Tk()
-        self.main_screen.geometry("480x270")
-        self.main_screen.title("Authorization")
+class AP2P(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-        Button(text="Login", height="2", width="30", command=self.login).pack()
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
 
-        Label(text="").pack()
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        Button(text="Register", height="2", width="30", command=self.register).pack()
+        self.frames = {}
 
-    def register(self):
-        self.register_screen = Toplevel(self.main_screen)
+        for F in (UserAuthPage, ProfilePage):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(UserAuthPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class UserAuthPage(tk.Frame):
+    def __init__(self, parent, controller):
+        self.parent = parent
+        self.controller = controller
+
+        tk.Frame.__init__(self, parent)
+
+        controller.geometry("480x270")
+        controller.title("Authorization")
+
+        button1 = ttk.Button(self, text="Login", command=self.login)
+        button1.place(relx=0.5, rely=0.35, relheight=0.2, relwidth=0.3, anchor="center")
+
+        button2 = ttk.Button(self, text="Register", command=self.signup)
+        button2.place(relx=0.5, rely=0.65, relheight=0.2, relwidth=0.3, anchor="center")
+
+    def signup(self):
+        self.register_screen = tk.Toplevel(self.controller)
         self.register_screen.title("Register")
         self.register_screen.geometry("400x175")
 
-        self.username_verify = StringVar()
-        self.password_verify = StringVar()
+        self.username_verify = tk.StringVar()
+        self.password_verify = tk.StringVar()
 
-        Label(self.register_screen, text="Username").pack()
-        self.username_register_entry = Entry(self.register_screen, textvariable=self.username_verify)
-        self.username_register_entry.pack()
+        label1 = tk.Label(self.register_screen, text="Username")
+        label1.place(relx=0.35, rely=0.2, relheight=0.2, relwidth=0.3, anchor="center")
+        self.username_register_entry = tk.Entry(self.register_screen, textvariable=self.username_verify)
+        self.username_register_entry.place(relx=0.65, rely=0.2, relheight=0.15, relwidth=0.3, anchor="center")
 
-        Label(self.register_screen, text="").pack()
+        label2 = tk.Label(self.register_screen, text="Password")
+        label2.place(relx=0.35, rely=0.5, relheight=0.2, relwidth=0.3, anchor="center")
+        self.password_register_entry = tk.Entry(self.register_screen, textvariable=self.password_verify, show='*')
+        self.password_register_entry.place(relx=0.65, rely=0.5, relheight=0.15, relwidth=0.3, anchor="center")
 
-        Label(self.register_screen, text="Password").pack()
-        self.password_register_entry = Entry(self.register_screen, textvariable=self.password_verify, show='*')
-        self.password_register_entry.pack()
-
-        Label(self.register_screen, text="").pack()
-
-        Button(self.register_screen, text="Register", width=10, height=1, command=self.register_verify).pack()
+        button = tk.Button(self.register_screen, text="Register", command=self.signup_verify)
+        button.place(relx=0.5, rely=0.8, relheight=0.2, relwidth=0.4, anchor="center")
 
     def login(self):
-        self.login_screen = Toplevel(self.main_screen)
+        self.login_screen = tk.Toplevel(self.controller)
         self.login_screen.title("Login")
         self.login_screen.geometry("400x175")
 
-        self.username_verify = StringVar()
-        self.password_verify = StringVar()
+        self.username_verify = tk.StringVar()
+        self.password_verify = tk.StringVar()
 
-        Label(self.login_screen, text="Username").pack()
-        self.username_login_entry = Entry(self.login_screen, textvariable=self.username_verify)
-        self.username_login_entry.pack()
+        label1 = tk.Label(self.login_screen, text="Username")
+        label1.place(relx=0.35, rely=0.2, relheight=0.2, relwidth=0.3, anchor="center")
+        self.username_login_entry = tk.Entry(self.login_screen, textvariable=self.username_verify)
+        self.username_login_entry.place(relx=0.65, rely=0.2, relheight=0.15, relwidth=0.3, anchor="center")
 
-        Label(self.login_screen, text="").pack()
+        label2 = tk.Label(self.login_screen, text="Password")
+        label2.place(relx=0.35, rely=0.5, relheight=0.2, relwidth=0.3, anchor="center")
+        self.password_login_entry = tk.Entry(self.login_screen, textvariable=self.password_verify, show='*')
+        self.password_login_entry.place(relx=0.65, rely=0.5, relheight=0.15, relwidth=0.3, anchor="center")
 
-        Label(self.login_screen, text="Password").pack()
-        self.password_login_entry = Entry(self.login_screen, textvariable=self.password_verify, show='*')
-        self.password_login_entry.pack()
+        button = tk.Button(self.login_screen, text="Login", command=self.login_verify)
+        button.place(relx=0.5, rely=0.8, relheight=0.2, relwidth=0.4, anchor="center")
 
-        Label(self.login_screen, text="").pack()
-
-        Button(self.login_screen, text="Login", width=10, height=1, command=self.login_verify).pack()
-
-    def register_verify(self):
+    def signup_verify(self):
         username = self.username_verify.get()
 
-        # Get list of usernames to check whether this user exists
-        list_of_files = os.listdir()
-        if username in list_of_files:
-            # User already exists
-            self.register_user_found()
-        else:
+        user_info = get_user_info(username)
+        if user_info is None:
             # Register new user
-            self.register_user()
-            self.register_success()
+            self.signup_user()
+            self.signup_success()
+        else:
+            # User already exists
+            self.signup_user_found()
 
-        self.username_register_entry.delete(0, END)
-        self.password_register_entry.delete(0, END)
+        self.username_register_entry.delete(0, tk.END)
+        self.password_register_entry.delete(0, tk.END)
 
     def login_verify(self):
         username = self.username_verify.get()
         password = self.password_verify.get()
+        self.username_login_entry.delete(0, tk.END)
+        self.password_login_entry.delete(0, tk.END)
 
         user_info = get_user_info(username)
         if user_info is None:
@@ -97,19 +127,22 @@ class UserAuthentication:
                 # Wrong password
                 self.password_not_recognised()
 
-        self.username_login_entry.delete(0, END)
-        self.password_login_entry.delete(0, END)
-
-    def register_success(self):
-        self.register_success_screen = Toplevel(self.register_screen)
+    def signup_success(self):
+        self.register_success_screen = tk.Toplevel(self.register_screen)
         self.register_success_screen.title("Success")
         self.register_success_screen.geometry("360x120")
 
-        Label(self.register_success_screen, text="Registration Success").pack()
+        label = tk.Label(self.register_success_screen, text="Registration Success")
+        label.place(relx=0.5, rely=0.35, anchor="center")
 
-        Button(self.register_success_screen, text="OK", command=self.delete_register_success).pack()
+        def register_close():
+            self.register_success_screen.destroy()
+            self.register_screen.destroy()
 
-    def register_user(self):
+        button = tk.Button(self.register_success_screen, text="OK", command=register_close)
+        button.place(relx=0.5, rely=0.65, anchor="center")
+
+    def signup_user(self):
         username = self.username_verify.get()
         password = self.password_verify.get()
 
@@ -125,53 +158,55 @@ class UserAuthentication:
         except subprocess.CalledProcessError as e:
             print("Error executing script:", e)
 
-    def register_user_found(self):
-        self.register_user_found_screen = Toplevel(self.register_screen)
+    def signup_user_found(self):
+        self.register_user_found_screen = tk.Toplevel(self.register_screen)
         self.register_user_found_screen.title("Error")
         self.register_user_found_screen.geometry("360x120")
-        Label(self.register_user_found_screen, text="User already exists").pack()
-        Button(self.register_user_found_screen, text="OK", command=self.delete_register_user_found_screen).pack()
+
+        label = tk.Label(self.register_user_found_screen, text="User already exists")
+        label.place(relx=0.5, rely=0.35, anchor="center")
+
+        button = tk.Button(self.register_user_found_screen, text="OK", command=self.register_user_found_screen.destroy)
+        button.place(relx=0.5, rely=0.65, anchor="center")
 
     def login_success(self):
-        self.login_success_screen = Toplevel(self.login_screen)
-        self.login_success_screen.title("Success")
-        self.login_success_screen.geometry("360x120")
-        Label(self.login_success_screen, text="Login Success").pack()
-        Button(self.login_success_screen, text="OK", command=self.delete_login_success).pack()
+        self.login_screen.destroy()
+        self.controller.show_frame(ProfilePage)
 
     def password_not_recognised(self):
-        self.password_not_recog_screen = Toplevel(self.login_screen)
+        self.password_not_recog_screen = tk.Toplevel(self.login_screen)
         self.password_not_recog_screen.title("Error")
         self.password_not_recog_screen.geometry("360x120")
-        Label(self.password_not_recog_screen, text="Invalid Password ").pack()
-        Button(self.password_not_recog_screen, text="OK", command=self.delete_password_not_recognised).pack()
+
+        label = tk.Label(self.password_not_recog_screen, text="Invalid Password")
+        label.place(relx=0.5, rely=0.35, anchor="center")
+
+        button = tk.Button(self.password_not_recog_screen, text="OK", command=self.password_not_recog_screen.destroy)
+        button.place(relx=0.5, rely=0.65, anchor="center")
 
     def user_not_found(self):
-        self.user_not_found_screen = Toplevel(self.login_screen)
+        self.user_not_found_screen = tk.Toplevel(self.login_screen)
         self.user_not_found_screen.title("Error")
         self.user_not_found_screen.geometry("360x120")
-        Label(self.user_not_found_screen, text="User not found").pack()
-        Button(self.user_not_found_screen, text="OK", command=self.delete_user_not_found_screen).pack()
 
-    def delete_register_success(self):
-        self.register_success_screen.destroy()
+        label = tk.Label(self.user_not_found_screen, text="User not found")
+        label.place(relx=0.5, rely=0.35, anchor="center")
 
-    def delete_register_user_found_screen(self):
-        self.register_user_found_screen.destroy()
+        button = tk.Button(self.user_not_found_screen, text="OK", command=self.user_not_found_screen.destroy)
+        button.place(relx=0.5, rely=0.65, anchor="center")
 
-    def delete_login_success(self):
-        self.login_success_screen.destroy()
 
-    def delete_password_not_recognised(self):
-        self.password_not_recog_screen.destroy()
+class ProfilePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
 
-    def delete_user_not_found_screen(self):
-        self.user_not_found_screen.destroy()
+        label = ttk.Label(self, text="Profile Page")
+        label.place(relx=0.5, rely=0.35, anchor="center")
 
-    def main_account_screen(self):
-        self.main_screen.mainloop()
+        button1 = ttk.Button(self, text="AuthPage", command=lambda: controller.show_frame(UserAuthPage))
+        button1.place(relx=0.5, rely=0.65, anchor="center")
 
 
 if __name__ == "__main__":
-    app = UserAuthentication()
-    app.main_account_screen()
+    app = AP2P()
+    app.mainloop()
